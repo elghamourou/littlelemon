dc4d3a45f240f4213a70165785f3b40420357457
elghamourou <elghamourou@gmail.com>
2025-08-13 14:00:30 +0100

    add class based API view for GET and POST MenuItem

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from restaurant.models import Booking
-from restaurant.serializers import BookingSerializer
+from restaurant.models import Booking, MenuItem
+from restaurant.serializers import BookingSerializer, MenuItemSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -19,3 +19,17 @@ class BookingView(APIView):
         bookings = Booking.objects.all()
         serializer = BookingSerializer(bookings, many=True)
         return Response(serializer.data)
+
+class MenuItemView(APIView):
+    def get(self, request):
+        menu_items = MenuItem.objects.all()
+        serializer = MenuItemSerializer(menu_items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=400)
