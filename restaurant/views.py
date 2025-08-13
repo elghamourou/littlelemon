6acee9a90865127267f15aacced59ea2731cb873
elghamourou <elghamourou@gmail.com>
2025-08-13 13:55:57 +0100

    add class based API view for GET Booking

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from restaurant.models import Booking
+from restaurant.serializers import BookingSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 
 def say_hello(request):
@@ -8,3 +13,9 @@ def say_hello(request):
 
 def home(request):
     return render(request, 'index.html',{})
+
+class BookingView(APIView):
+    def get(self, request):
+        bookings = Booking.objects.all()
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
