f33f86f88d90fc0e541086b012758c29d26d55e9
elghamourou <elghamourou@gmail.com>
2025-08-13 08:21:24 +0100

    registring models Booking and Menu to django admin site

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
+admin.site.site_header = "Little Lemon Admin"
+admin.site.site_title = "Little Lemon Admin Portal"
+admin.site.index_title = "Welcome to Little Lemon Restaurant Portal"
 # Register your models here.
+
+
+
+admin.site.register(Booking)
+admin.site.register(Menu)
