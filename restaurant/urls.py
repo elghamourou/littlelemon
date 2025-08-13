6242a5d443a5ce6e126ad345d0ea7a83dd5cfe36
elghamourou <elghamourou@gmail.com>
2025-08-13 14:03:43 +0100

    add routes for API endpoints in urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,4 +8,6 @@ from . import views
 urlpatterns = [
     path('hello', views.say_hello, name='say_hello'),
     path('', views.home, name='home'),
+    path('bookings/', views.BookingView.as_view()),
+    path('menu/', views.MenuItemView.as_view()),
 ]
